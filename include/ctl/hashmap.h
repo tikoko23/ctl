b6da925d2356e9bbffd0eed691afec3ddc28e506
tikoko23 <code.tikoko@gmail.com>
2025-08-07 21:00:54 +0300

    added hashmap documentation

--- include/ctl/hashmap.h
+++ include/ctl/hashmap.h
@@ -14,18 +14,45 @@ typedef struct {
     _THashmapItem *items;
 } _THashmapBucket;
 
+/**
+ * \ref "THashmap" is a hashmap implementation which hashes \ref "TStringView"
+ * keys using FNV-1a.
+ */
 typedef struct {
-    size_t n_buckets;
-    _THashmapBucket *buckets;
-    TCleanup item_destructor;
+    size_t n_buckets;           /**< Length of the bucket array */
+    _THashmapBucket *buckets;   /**< The bucket array */
+    TCleanup item_destructor;   /**< Callback function to be called when a key is overwritten or erased */
 } THashmap;
 
+/**
+ * Creates a hashmap with `n_buckets` buckets.
+ */
 THashmap tHashmapNew(size_t n_buckets);
+
+/**
+ * Creates a hashmap with `n_buckets` buckets and a destructor.
+ * \param destructor A callback to be called when a key's value is overwritten or the key is erased
+ */
 THashmap tHashmapNewCb(size_t n_buckets, TCleanup destructor);
 
+/**
+ * Sets a key's value in the hashmap.
+ * If the key doesn't exist, it creates it with the value `data`.
+ * If the key exists, the value is replaced with `data`.
+ * During replacement, `this->destructor` will be called if it is not `NULL`.
+ * If `data` is `NULL`, the key is removed from the hashmap.
+ */
 void tHashmapSet(THashmap *this, TStringView key, void *data);
+
+/**
+ * Returns the value associated with `key`, or `NULL` if it does not exist.
+ */
 void *tHashmapGet(const THashmap *this, TStringView key);
 
+/**
+ * Destructs all values and deallocates all memory associated with `this`.
+ * If `this->destructor` is not `NULL`, it is invoked for every value.
+ */
 void tHashmapFree(THashmap *this);
 
 #endif
